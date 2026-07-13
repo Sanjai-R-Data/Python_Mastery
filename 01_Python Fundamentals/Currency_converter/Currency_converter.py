6b1e431a74f6bd2640ae0b5357047eca36eeca9f
Sanjai2008-tech <sanjairavi0308@gmail.com>
2026-07-13 19:17:39 +0530

    Currency converter

---
+++ 01_Python Fundamentals/Currency_converter/Currency_converter.py
@@ -0,0 +1,15 @@
+# Currency(INR to USD)
+
+inr = float(input("Enter Amount(in INR): "))
+
+usd = inr/95.66
+
+print("Amount(in usd): ",round(usd,2))
+
+# Currency(USD to INR)
+
+usdd = float(input("\nEnter Amount(in USD): "))
+
+inrr = usdd*95.66
+
+print("Amount(in inr): ",round(inrr,2))
