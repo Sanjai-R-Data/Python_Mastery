faccf132e6e6c8a59a6df0de84c578173f7bfb2d
Sanjai2008-tech <sanjairavi0308@gmail.com>
2026-07-15 18:00:12 +0530

    grade calculator

---
+++ 02_Loops and Conditional Statements/Grade Calculator/grade_calculator.py
@@ -0,0 +1,18 @@
+#i grade calculator using loops and conditions
+
+
+
+marks = float(input("Enter Marks: "))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+elif marks >= 60:
+    print("Grade: D")
+elif marks >= 50:
+    print("Grade: E")
+else:
+    print("Grade: F")
