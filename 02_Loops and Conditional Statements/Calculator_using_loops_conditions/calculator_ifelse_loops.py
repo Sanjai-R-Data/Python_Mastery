3c58d7c2eb9306346bd46ff72e7e15ee106c4e8c
Sanjai2008-tech <sanjairavi0308@gmail.com>
2026-07-15 17:55:20 +0530

    Calculator using loops and conditions

---
+++ 02_Loops and Conditional Statements/Calculator_using_loops_conditions/calculator_ifelse_loops.py
@@ -0,0 +1,32 @@
+# Calculator using loops and conditional statements
+
+
+while True:
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 5:
+        print("Thank You!")
+        break
+
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+
+    if choice == 1:
+        print("Result =", a + b)
+    elif choice == 2:
+        print("Result =", a - b)
+    elif choice == 3:
+        print("Result =", a * b)
+    elif choice == 4:
+        if b != 0:
+            print("Result =", a / b)
+        else:
+            print("Cannot divide by zero")
+    else:
+        print("Invalid Choice")
